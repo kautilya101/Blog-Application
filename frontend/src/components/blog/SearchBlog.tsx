c2b5977b98790d0cc02dea868fae14f58b7f76d7
kautilya101 <kautilya101001@gmail.com>
2024-08-25 11:44:48 +0530

    fix: unnecessary code removed

--- frontend/src/components/blog/SearchBlog.tsx
+++ frontend/src/components/blog/SearchBlog.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react'
+import { useContext, useState } from 'react'
 import { FaSearch } from 'react-icons/fa'
 import useDebounce from '../../Hooks/useDebounce'
 import { BlogContext } from '../../context/BlogContextProvider'
