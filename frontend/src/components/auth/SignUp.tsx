4922d4cb6a1e4ad1372d20ab0033d846aaab17f9
kautilya101 <kautilya101001@gmail.com>
2024-08-25 12:04:10 +0530

    fix: unnecessary code removed

--- frontend/src/components/auth/SignUp.tsx
+++ frontend/src/components/auth/SignUp.tsx
@@ -15,7 +15,7 @@ export default function SignUp() {
     setLoading(true);
     const data = {username:username, email: email, password: password};
     try{
-      const response = await fetch(url+'/api/user/signIn',{
+      const response = await fetch(url+'/api/user/signUp',{
         method: 'POST',
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(data),
